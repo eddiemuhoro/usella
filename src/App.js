09a8f01302d16f496a6fe518a0ce2190d6cfc355
eddiemuhoro <eddiemuhoro@gmail.com>
2023-03-06 16:45:10 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
+import Navbar from './components/navbar/Navbar';
+import Login from './components/register/Login';
 import Register from './components/register/Register';
 function App () {
   return (
     <div className="App">
-      <Register />
+      <Navbar />
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/' element={<>Home page here</>} />
+      </Routes>
     </div>
   );
 }
