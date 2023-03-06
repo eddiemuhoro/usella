97defa5a723ab0d0a00047455a2548d74b0af445
eddiemuhoro <eddiemuhoro@gmail.com>
2023-03-06 23:00:03 +0300

    added products page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/navbar/Navbar';
+import Products from './components/products/Products';
 import Login from './components/register/Login';
 import Register from './components/register/Register';
 function App () {
@@ -12,6 +13,8 @@ function App () {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path='/' element={<>Home page here</>} />
+        <Route path='/products' element={<Products/>} />
+        <Route path='*' element={<>404 - Not found</>} />
       </Routes>
     </div>
   );
