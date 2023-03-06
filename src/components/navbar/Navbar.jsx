0d72476982e1ac4e1f192e6d266c4cae81041123
eddiemuhoro <eddiemuhoro@gmail.com>
2023-03-06 16:45:24 +0300

    added navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Navbar.css";
+import {AiOutlineShoppingCart} from 'react-icons/ai';
+import {AiOutlineMenu} from 'react-icons/ai';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import { useDispatch } from "react-redux";
+import { logout, reset } from "../../react-redux/features/auth/authSlice";
+
+function Navbar() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [click, setClick] = useState(false);
+
+  const onLogout = ()=>{
+    dispatch(logout())
+    dispatch(reset())
+    alert('You are now logged out')
+    navigate('/login')
+    window.location.reload()
+}
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+          MyShop
+        </Link>
+
+        <div className="menu-icon" onClick={handleClick}>
+          <AiOutlineMenu />
+        </div>
+
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/products" className="nav-links" onClick={closeMobileMenu}>
+              Products
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-links" onClick={closeMobileMenu}>
+              Contact
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/cart" className="nav-links" onClick={closeMobileMenu}>
+              <AiOutlineShoppingCart />
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link onClick={onLogout} className="nav-links" >
+              <LogoutRoundedIcon />
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
