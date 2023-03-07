54c1ae1cfaf3898c0e4b7707967dc38a5f2fac1b
eddiemuhoro <eddiemuhoro@gmail.com>
2023-03-07 17:07:25 +0300

    added heros page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
+import Hero from './components/home/Hero';
 import Navbar from './components/navbar/Navbar';
 import Products from './components/products/Products';
 import Login from './components/register/Login';
@@ -12,7 +13,7 @@ function App () {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path='/' element={<>Home page here</>} />
+        <Route path='/' element={<Hero/>} />
         <Route path='/products' element={<Products/>} />
         <Route path='*' element={<>404 - Not found</>} />
       </Routes>
