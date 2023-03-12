ddb22ed13b261f30fb9cdaffb43593aca62335ac
eddiemuhoro <eddiemuhoro@gmail.com>
2023-03-12 23:25:27 +0300

    changed authentication api from local to live

--- src/react-redux/features/auth/authService.js
+++ src/react-redux/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:9000/';
+const API_URL = 'https://odd-slip-ant.cyclic.app/';
 
 const register = async(userData)=>{
     const response = await axios.post(API_URL + 'register', userData)
