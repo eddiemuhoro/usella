9c83c61c5d598408bf97a023a755943de306b5f8
eddiemuhoro <eddiemuhoro@gmail.com>
2023-03-19 23:12:18 +0300

    added details about seller

--- src/components/products/SingleProduct.js
+++ src/components/products/SingleProduct.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { BsCart3, BsHeart, BsHeartFill } from 'react-icons/bs';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import { addToCart, getProductByCategory, getProductById } from '../../react-redux/features/products/productSlice';
+import { addToCart, getCart, getProductByCategory, getProductById, getProductByUser } from '../../react-redux/features/products/productSlice';
 import CartButton from './CartButton';
 import Wishlist from './WishlistButton';
 
@@ -11,13 +11,12 @@ import Wishlist from './WishlistButton';
 const SingleProduct = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
-  //save update state in local storage
  const [update , setUpdate] = useState(false)
-
   const user = useSelector(state => state.auth.you)
   const params = useParams()
   const { id } = params
-  const [color, setColor] = useState('#000000'); // initial color is black
+  const [color, setColor] = useState('#ffffff'); // initial color is black
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -28,19 +27,16 @@ const SingleProduct = () => {
   }, []);
 
   function getRandomColor() {
-    var letters = "0123456789ABCDEF";
+    var letters = "123456789ABCDEF";
     var color = "#";
     for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+      color += letters[Math.floor(Math.random() * 15)];
     }
     return color;
   }
 
   //fetch clicked product'
   const [products, setclickedProduct] = useState([])
-
-
-
 
 const handleCart =  () => {
   const cartData ={
@@ -62,6 +58,18 @@ const handleCart =  () => {
 
 
       const [cart, setCart] = useState([])
+      //fetch cart data by user id
+      useEffect(() => {
+        dispatch(getCart(products.id))
+        .then(res => {
+          setCart(res.payload)
+        }
+        )
+    }, [dispatch, products.id])
+
+
+
+
       useEffect(() => {
         dispatch(getProductById(id))
         .then(res => {
@@ -72,12 +80,33 @@ const handleCart =  () => {
 
       const [category , setCategory] = useState([ ])
       useEffect(() => {
+        setLoading(true)
         dispatch(getProductByCategory(products.category))
         .then(res => {
           setCategory(res.payload)
+          setLoading(false)
         }
         )
       }, [dispatch, products.category])
+
+
+      const [sellerProducts , setSellerProducts] = useState([])
+      useEffect(() => {
+        setLoading(true)
+        dispatch(getProductByUser(products.sellerId))
+        .then(res => {
+          setSellerProducts(res.payload)
+          setLoading(false)
+        })
+        // const fetchProducts = async () => {
+        //   const { data } = await axios.get(`http://localhost:9000/products/seller/${seller}`)
+        //   setProducts(data)
+        // }
+        // fetchProducts()
+      }, [dispatch, products.sellerId])
+
+
+
 
       const cutDescription = (description) => {
         if (description.length > 50) {
@@ -91,10 +120,17 @@ const handleCart =  () => {
       const handleReload = () => {
         setTimeout(() => {
           window.location.reload()
-        }, 1000);
+        }, 500);
       }
 
-
+      const cutSellerDescription = (description) => {
+        if (description.length > 30) {
+          return description.substring(0, 30) + '...'
+        }else if(description.length < 30) {
+            return description
+        }
+      }
+  
  
   
   return (
@@ -113,18 +149,27 @@ const handleCart =  () => {
                 <h2 className="info-price">${products.price}</h2>
               </div>
               <div className='warning'>
-                <p style={{color, transition: 'color 2s ease-in-out' }}>3 items left</p>
+                <p style={{color, transition: 'color 2s ease-in-out' }}>{products.quantity} items left</p>
               </div>
               <div className='cart-button'>
                 
                 {/* CONDITIONAL RENDERING */}
+               
+                {/* IF QUANTITY IS 0, DISABLE ADD TO CART BUTTON */}
                 {
-                  (cart.length === 0 || cart[0].userId !== user.id) && !update  ? (
-                    <button onClick={handleCart}>Add to cart</button>
-                  ) : (
-                    <Link to='/cart' ><button>Already added to cart</button></Link>
+                  products.quantity === 0 ?  (<button title='out of stock' disabled>Add to cart</button>):
+                  (
+                    
+                      (cart.length === 0 || cart[0].userId !== user.id) && !update  ? (
+                        <button onClick={handleCart}>Add to cart</button>
+                      ) : (
+                        <Link to='/cart' ><button>Already added to cart</button></Link>
+                      )
+                    
                   )
                 }
+
+             
               </div>
               <div className='favorite'>
                   {/* DISPLAY WISHLISsT ID */}
@@ -143,13 +188,28 @@ const handleCart =  () => {
       }
         
         <section className='seller-info'>
-          <h2>Henry</h2>
-          <p style={{textDecoration:'underline'}}>Other products posted by henry[8]</p>
+         
+          <h4 >Other products posted by {products.sellerName}</h4>
+          <div className='seller-products'>
+          {loading && <p>Please wait a sec...</p>}
+              {
+                sellerProducts.map(product => (
+                 <ul>
+                    <li>
+                      <Link key={product.id} onClick={handleReload} to={`/products/${product.id}`}>
+                        <p ><span style={{textDecoration:'underline'}}>{product.name}</span> {cutSellerDescription(product.description)}</p>
+                      </Link>
+                    </li>
+                 </ul>
+                ))
+              }
+          </div>
         </section>
     </div>
-
+    <h2>Related products</h2>
     <section className="products">
-    
+     
+    {loading && <p>Please wait a sec...</p>}
     {
       category.map(product => (
         <Link key={product.id} onClick={handleReload} to={`/products/${product.id}`}>
