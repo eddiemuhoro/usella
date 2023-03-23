9907a43ab9e0e5c6230dd8aba950c6e0c312ad20
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-03-23 20:50:06 +0300

    ADD: Added new files

--- backend/src/User/user.ts
+++ backend/src/User/user.ts
@@ -1,28 +1,79 @@
-
+import { body } from 'express-validator';
+import { handleErrors } from '../middleware/handleErrors';
+import { prisma } from '../db';
+import bcrypt from 'bcrypt';
+import { Request, Response } from 'express';
 //* register user
 
-export const register = (
-    async (res : any, req : any) => {
-        try{
-            req.send("register user")
-
-        }catch(e: any){
-            req.status(500).send({message: e.message})
+export const register =
+  (body('name').isString(),
+  body('email').isEmail(),
+  body('phone').isString(),
+  body('password').isString(),
+  body('location').isString(),
+  handleErrors,
+  async (req: Request, res: Response) => {
+    try {
+      const { name, email, phone, password, location } = req.body;
+      const user = await prisma.user.create({
+        data: {
+          name: name,
+          email: email,
+          phone: phone,
+          password: bcrypt.hashSync(password, 10),
+          location: location
         }
-    }
-    
-); 
+      });
 
+      if (!user) {
+        res.status(500).json('could not create user');
+      }
+
+      // //* register the profile of the user
+      const profile = await prisma.profile.create({
+        data: {
+          id: user.id
+        }
+      });
+
+      if (!profile) {
+        res.status(500).json({ message: 'user profile not created' });
+      }
+
+      res.json({ message: 'user created successfully', id: user.id });
+    } catch (e: any) {
+      res.status(500).send({ message: e.message });
+    }
+  });
 
 //* login user
 
-export const login = (
-    async (res : any, req: any) => {
-        try{
-            req.send("login user")
-
-        }catch(e: any){
-            req.status(500).send({message: e.message})
+export const login =
+  (body('email').isString(),
+  body('password').isString(),
+  handleErrors,
+  async (req: Request, res: Response) => {
+    try {
+      const { email, password } = req.body;
+      const user = await prisma.user.findUnique({
+        where: {
+          email: email
         }
+      });
+      if (!email) {
+        res
+          .status(404)
+          .json({ message: 'make sure you provide the right credentials' });
+      }
+
+      const isValid = await bcrypt.compare(password, user!.password);
+
+      if (!isValid) {
+        res.status(500).json({ message: 'Invalid credentials' });
+      }
+
+      res.json({ message: 'User logged in successfully', id: user!.id });
+    } catch (e: any) {
+      res.status(500).send({ message: e.message });
     }
-); 
+  });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,32 @@
 import express from 'express';
 import 'dotenv/config';
-import morgan from 'morgan'
-import cors from 'cors'
-import { login, register } from './User/user.ts';
+import morgan from 'morgan';
+import cors from 'cors';
+import { login, register } from './User/user';
+import bodyParser from 'body-parser';
+import productRouter from './product/product';
 // import {login, register} from './User/user';
-
 
 const app = express();
 
-const port = process.env.PORT || 4300;
+const port = process.env.PORT || 4200;
 
 //* creating default middleware for the app
-app.use(express.json())
-app.use(morgan("dev"))
-app.use(cors())
-
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //* routes
-app.all('/', (_req, res)=>{
-    res.send("welcome to usella, where send hand good has value")
-})
+app.all('/', (_req, res) => {
+  res.send('welcome to usella, where send hand good has value');
+});
 
 app.use('/login', login);
-app.use('/register', register)
-
+app.use('/register', register);
+app.use('/product', productRouter);
 
 //* create listen app
-app.listen(port, ()=>{
-    console.log(`Server is listening in port : ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is listening in port : ${port}`);
+});
