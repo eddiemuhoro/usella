b1f377d2f9fd1191c885ef57f48660dc1de2ef90
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-03-27 01:45:41 +0300

    ADD: aadded new files

--- backend/src/swagger/swagger.ts
+++ backend/src/swagger/swagger.ts
@@ -1,20 +1,20 @@
-import swaggerJSDoc from 'swagger-jsdoc';
+// import swaggerJSDoc from 'swagger-jsdoc';
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    info: {
-      title: 'My API',
-      version: '1.0.0',
-      description: 'My API documentation'
-    },
-    servers: [
-      {
-        url: 'http://localhost:3000',
-        description: 'Development server'
-      }
-    ]
-  },
-  apis: ['./src/*.ts'] // Path to the API routes folder
-};
+// const swaggerOptions = {
+//   swaggerDefinition: {
+//     info: {
+//       title: 'My API',
+//       version: '1.0.0',
+//       description: 'My API documentation'
+//     },
+//     servers: [
+//       {
+//         url: 'http://localhost:3000',
+//         description: 'Development server'
+//       }
+//     ]
+//   },
+//   apis: ['./src/*.ts'] // Path to the API routes folder
+// };
 
-export const swaggerDocs = swaggerJSDoc(swaggerOptions);
+// export const swaggerDocs = swaggerJSDoc(swaggerOptions);
