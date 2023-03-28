934cb4925ccbc3bd6e88f5c31a25c3ee4bc6bcf4
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-03-28 12:25:33 +0300

    FIX: returned details for the user

--- backend/src/User/user.ts
+++ backend/src/User/user.ts
@@ -9,45 +9,51 @@ const router = Router();
 
 export const register =
   (body('name').isString(),
-    body('email').isEmail(),
-    body('phone').isString(),
-    body('password').isString(),
-    body('location').isString(),
-    handleErrors,
-
-    async (req: Request, res: Response) => {
-      try {
-        const { name, email, phone, password, location } = req.body;
-        const user = await prisma.user.create({
-          data: {
-            name: name,
-            email: email,
-            phone: phone,
-            password: bcrypt.hashSync(password, 10),
-            location: location
-          }
-        });
-
-        if (!user) {
-          res.status(500).json('could not create user');
+  body('email').isEmail(),
+  body('phone').isString(),
+  body('password').isString(),
+  body('location').isString(),
+  handleErrors,
+  async (req: Request, res: Response) => {
+    try {
+      const { name, email, phone, password, location } = req.body;
+      const user = await prisma.user.create({
+        data: {
+          name: name,
+          email: email,
+          phone: phone,
+          password: bcrypt.hashSync(password, 10),
+          location: location
+        },
+        select:{
+          name: true,
+          id: true,
+          email: true,
+          phone: true,
+          location: true,
         }
+      });
 
-        // //* register the profile of the user
-        const profile = await prisma.profile.create({
-          data: {
-            id: user.id
-          }
-        });
-
-        if (!profile) {
-          res.status(500).json({ message: 'user profile not created' });
-        }
-
-        res.status(200).json({ message: 'user created successfully', id: user.id });
-      } catch (e: any) {
-        res.status(500).send({ message: e.message });
+      if (!user) {
+        res.status(500).json('could not create user');
       }
-    });
+
+      // //* register the profile of the user
+      const profile = await prisma.profile.create({
+        data: {
+          id: user.id
+        }
+      });
+
+      if (!profile) {
+        res.status(500).json({ message: 'user profile not created' });
+      }
+
+      res.status(200).json(user);
+    } catch (e: any) {
+      res.status(500).send({ message: e.message });
+    }
+  });
 
 //* login user
 
@@ -70,44 +76,45 @@ export const register =
  *         password:
  *          type: string
  *          example: "pass1234"
-*        responses:
-*             200:
-*             description: User logged in successfully
- * 
+ *        responses:
+ *             200:
+ *             description: User logged in successfully
+ *
  */
 
 export const login =
   (body('email').isString(),
-    body('password').isString(),
-    handleErrors,
-    async (req: Request, res: Response) => {
-      try {
-        const { email, password } = req.body;
-        const user = await prisma.user.findUnique({
-          where: {
-            email: email
-          }
-        });
-        if (!email) {
-          res
-            .status(404)
-            .json({ message: 'make sure you provide the right credentials' });
+  body('password').isString(),
+  handleErrors,
+  async (req: Request, res: Response) => {
+    try {
+      const { email, password } = req.body;
+      const user = await prisma.user.findUnique({
+        where: {
+          email: email
         }
-
-        const isValid = await bcrypt.compare(password, user!.password);
-
-        if (!isValid) {
-          res.status(500).json({ message: 'Invalid credentials' });
-        }
-
-        res.status(200).json({ message: 'User logged in successfully', id: user!.id });
-      } catch (e: any) {
-        res.status(500).send({ message: e.message });
+      });
+      if (!email) {
+        res
+          .status(404)
+          .json({ message: 'make sure you provide the right credentials' });
       }
-    });
+
+      const isValid = await bcrypt.compare(password, user!.password);
+
+      if (!isValid) {
+        res.status(500).json({ message: 'Invalid credentials' });
+      }
+
+      res
+        .status(200)
+        .json({ message: 'User logged in successfully', id: user!.id });
+    } catch (e: any) {
+      res.status(500).send({ message: e.message });
+    }
+  });
 
 //* fetch all the users
-
 
 router.get('/', async (_req: Request, res: Response) => {
   try {
@@ -150,8 +157,7 @@ router.all('/:id', async (req: Request, res: Response) => {
         profile: true
       }
     });
-     if(!user){
-
+    if (!user) {
       throw new Error('User not found');
     }
     res.status(200).json(user);
@@ -159,7 +165,6 @@ router.all('/:id', async (req: Request, res: Response) => {
     res.status(500).json({ message: e.message });
   }
 });
-
 
 //* delete account
 
@@ -180,6 +185,5 @@ router.all('/delete/:id', async (req: Request, res: Response) => {
     res.status(500).json({ message: e.message });
   }
 });
-
 
 export default router;
