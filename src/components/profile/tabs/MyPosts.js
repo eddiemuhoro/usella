fa6dedf025a18804a8885e719e647c1b871fc170
eddiemuhoro <eddiemuhoro@gmail.com>
2023-03-31 23:29:11 +0300

    added alighnements

--- src/components/profile/tabs/MyPosts.js
+++ src/components/profile/tabs/MyPosts.js
@@ -12,85 +12,90 @@ import { deleteProduct, getProductByUser } from '../../../react-redux/features/p
 const MyPosts = () => {
   const params = useParams()
   const dispatch = useDispatch()
-    const [products, setProducts] = useState([])
-    const user = useSelector(state => state.auth.you)
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(false)
+  const user = useSelector(state => state.auth.you)
 
-    const seller = user.id
+  const seller = user.id
 
-    console.log(seller)
+  console.log(seller)
 
-    //state to update the fetched products using useEffect
-    const [update, setUpdate] = useState(false)
-  
-      //delete product with a confirmation message
-  const handleDelete =  (id, name) => {
+  //state to update the fetched products using useEffect
+  const [update, setUpdate] = useState(false)
+
+  //delete product with a confirmation message
+  const handleDelete = (id, name) => {
     if (window.confirm(`Are you sure you want to delete ${name}?`)) {
       dispatch(deleteProduct(id))
-      .then(res => {
-        setUpdate(true)
-      }
-      )
+        .then(res => {
+          setUpdate(true)
+        }
+        )
     }
   }
 
-    //fetxh from redux store
-    useEffect(() => {
-      dispatch(getProductByUser(seller))
+  //fetxh from redux store
+  useEffect(() => {
+    setLoading(true)
+    dispatch(getProductByUser(seller))
       .then(res => {
         setProducts(res.payload)
         setUpdate(false)
+        setLoading(false)
       })
 
-      // const fetchProducts = async () => {
-      //   const { data } = await axios.get(`http://localhost:9000/products/seller/${seller}`)
-      //   setProducts(data)
-      // }
-      // fetchProducts()
-    }, [update, dispatch, seller])
+    // const fetchProducts = async () => {
+    //   const { data } = await axios.get(`http://localhost:9000/products/seller/${seller}`)
+    //   setProducts(data)
+    // }
+    // fetchProducts()
+  }, [update, dispatch, seller])
 
-    console.log(products)
+  console.log(products)
 
-    
+
 
 
   return (
     <div>
-         <section className="products myProducts">
-          {
-            !products ? (
-              <div className="no-products">
-                <h1>You have no products</h1>
-                <Link to='/post' style={{textDecoration:'underline'}}>Add a product</Link>
+      <section className="products myProducts">
+        {
+          !products ? (
+            <div className="no-products">
+              <h1>You have no products</h1>
+              <Link to='/post' style={{ textDecoration: 'underline' }}>Add a product</Link>
+            </div>
+          ) : (
+
+            products.map(product => (
+              <div className="product">
+                <div className="product-img">
+                  {!loading ? (<img src={product.images[0]} alt="product" />
+                  ) : (<img src='https://media.istockphoto.com/id/1138824305/vector/loading-icon-on-black.jpg?s=170667a&w=0&k=20&c=5TgSExGSoy7SXYcXEKfKCfZW-qFXsTaZRHcBF99WMLM=' alt='loading' className='product-image' />
+                  )}
+                </div>
+                <div className="product-info">
+                  <p className="info-name">{product.name}</p>
+                  <p className="info-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+
+                </div>
+                <div className="product-btns">
+                  <p className="info-price">${product.price}</p>
+                  <p><AiFillDelete style={{ color: 'red' }} onClick={() => handleDelete(product.id, product.name)} /></p>
+                </div>
+                <div className='favorite'>
+                  <AiFillEdit style={{ color: '4b59f7' }} />
+                </div>
+
               </div>
-            ) : (
-              
-                products.map(product => (
-                  <div className="product">
-                      <div className="product-img">
-                        <img src={product.images[0]} alt="product" />
-                      </div>
-                      <div className="product-info">
-                        <p className="info-name">{product.name}</p>
-                        <p className="info-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
-                        
-                      </div>
-                      <div className="product-btns">
-                        <p className="info-price">${product.price}</p>
-                        <p><AiFillDelete style={{color:'red'}} onClick={() => handleDelete(product.id, product.name)} /></p>
-                      </div>
-                      <div className='favorite'>
-                        <AiFillEdit style={{color:'4b59f7'}} />
-                      </div>
-                      
-                  </div>
-                )
-              )
-            
             )
-          }
-        
+            )
+
+          )
+        }
+
       </section>
-      <Link to='/post' style={{textDecoration:'underline'}}>Sell</Link>
+      <Link to='/post' style={{ textDecoration: 'underline' }}>Sell</Link>
     </div>
   )
 }
