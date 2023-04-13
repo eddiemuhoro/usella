d6feb7a4701716c7a8417a095dd33b3155b2755b
eddiemuhoro <eddiemuhoro@gmail.com>
2023-04-13 21:01:06 +0300

    added filters

--- src/components/products/ProductDisplay.js
+++ src/components/products/ProductDisplay.js
@@ -50,6 +50,14 @@ const Product = ({ product }) => {
      navigate('/login') 
    }
 
+   const cutDescription = (description) => {
+    if (description.length > 50) {
+      return description.substring(0, 50) + '...'
+    }else if(description.length < 50) {
+      return description
+    }
+  }
+
   return (
     <div className="product">
       <div className="product-img">
@@ -61,8 +69,8 @@ const Product = ({ product }) => {
       <Link key={product.id} to={`/products/${product.id}`}>
 
         <div className="product-info">
-          <p className="info-name">{product.name}</p>
-          <p className="info-description">hsdkjc  sbvfhsv shvchjsjb shvhjsbkdhcbsh </p>
+          <p className="info-name">{product.category}</p>
+          <p className="info-description">{cutDescription(product.description)} </p>
         </div>
       </Link>
       <div className="product-btns">
@@ -96,19 +104,26 @@ const Product = ({ product }) => {
   );
 };
 
-const Products = ({ products }) => {
+const Products = ({ products, query, filteredData }) => {
   
   return (
     <div className="products">
       {
         products.length === 0 && ( <h2>No products found</h2>)
       }
-      {products.map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
+    
+      {
+        filteredData.map(product => (
+          <Product key={product.id} product={product} />
+        ))
+      }
     </div>
   );
 };
+
+
+
+
 
 const ProductDisplay = () => {
     const dispach = useDispatch();
@@ -127,11 +142,108 @@ const ProductDisplay = () => {
       }, [])
 
 
+      useEffect(() => {
+        // Calculate the maximum price in the data
+        const max = Math.max(...products.map(item => item.price));
+        setMaxPrice(max);
+        // Initialize the price range to the full range
+        setPriceRange([0, max]);
+      }, [products]);
+
+
+      const [query, setQuery] = useState('')
+      const [categoryFilter, setCategoryFilter] = useState('');
+      const [priceRange, setPriceRange] = useState([0, 100]);
+      const [maxPrice, setMaxPrice] = useState(0);
+
+      const filteredData = products.filter(item => {
+         // Apply the search text filter
+      if (query && !item.name.toLowerCase().includes(query.toLowerCase())) {
+        return false;
+      }
+        // Apply the category filter
+        if (categoryFilter && item.category !== categoryFilter) {
+          return false;
+        }
+
+          // Apply the price range filter
+      if (item.price < priceRange[0] || item.price > priceRange[1]) {
+        return false;
+      }
+        return true;
+      });
+
+
+      const handleCategoryFilterChange = (event) => {
+        setCategoryFilter(event.target.value);
+      };
+
+      const handlePriceRangeChange = (event) => {
+        setPriceRange([event.target.valueAsNumber, priceRange[1]]);
+      };
+    
+      const handlePriceRangeInputChange = (event) => {
+        setPriceRange([priceRange[0], event.target.valueAsNumber]);
+      };
+
+      const [showFilters, setShowFilters] = useState(false);
+
+  const handleToggleFilters = () => {
+    setShowFilters(!showFilters);
+  };
+
 
   return (
     <div className="app">
-      <h1>Products</h1>
-      <Products products={products} />
+      <div className='products-header'>
+        <h1>Products</h1>
+        <div className="search-input">
+          <input type="text" placeholder="Search..." onChange={(e) => setQuery(e.target.value)} />         
+        </div>
+
+        <button className="toggle-filters-button" onClick={handleToggleFilters}>Filters</button>
+
+        {
+          showFilters && (
+            <div className="filters">
+              <select value={categoryFilter} onChange={handleCategoryFilterChange}>
+                <option value="">All categories</option>
+                <option value="ELECTRONICS">ELECTRONICS</option>
+                <option value="FASHION">FASHION</option>
+                <option value="SPORTS">SPORTS</option>
+                <option value="HEALTH">HEALTH</option>
+
+              </select>
+              <input type="range" min={0} max={maxPrice} value={priceRange[0]} onChange={handlePriceRangeChange} />
+              <input type="range" min={0} max={maxPrice} value={priceRange[1]} onChange={handlePriceRangeInputChange} />
+              <p>Price range: {priceRange[0]} - {priceRange[1]}</p>
+            </div>
+          )
+        }
+
+        <div className='filters-desktop'>
+        <select value={categoryFilter} onChange={handleCategoryFilterChange}>
+          <option value="">All categories</option>
+          <option value="ELECTRONICS">ELECTRONICS</option>
+          <option value="FASHION">FASHION</option>
+          <option value="SPORTS">SPORTS</option>
+          <option value="HEALTH">HEALTH</option>
+
+        </select>
+        <input type="range" min={0} max={maxPrice} value={priceRange[0]} onChange={handlePriceRangeChange} />
+        <input type="range" min={0} max={maxPrice} value={priceRange[1]} onChange={handlePriceRangeInputChange} />
+        <p>Price range: {priceRange[0]} - {priceRange[1]}</p>
+  </div>
+      </div>
+      
+      <Products products={products} query={query}  filteredData={filteredData} />
+      {
+        filteredData.length === 0 && (
+          <div className='no-products'>
+            <img src='https://media.tenor.com/IHdlTRsmcS4AAAAC/404.gif' alt='404' />
+          </div>
+        )
+      }
     </div>
   );
 };
