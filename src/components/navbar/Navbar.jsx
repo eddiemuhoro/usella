60b220e6b2ae5a89130d1eb62957c07494d4d2b9
eddiemuhoro <eddiemuhoro@gmail.com>
2023-04-14 11:38:34 +0300

    changed site name

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -42,7 +42,7 @@ function Navbar() {
     <nav className="navbar">
       <div className="navbar-container">
         <NavLink to="/" className="navbar-logo" onClick={closeMobileMenu}>
-          MyShop
+          uSella
         </NavLink>
 
         <div className="menu-icon" onClick={handleClick}>
